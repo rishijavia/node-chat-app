0a9fbe4364fd424f0abcacf86636b9eec05d57c2
rishijavia <rishi.javia.3895@gmail.com>
2017-01-10 21:56:36 +0530

    Add geolocation support via geolocation API

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,21 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () =>{
     var message = generateMessage('Rishi', 'Yo what up?');
     expect(message.from).toBe('Rishi');
     expect(message.text).toBe('Yo what up?');
+    expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var message = generateLocationMessage('Rishi', 123, 321);
+    expect(message.from).toBe('Rishi');
+    expect(message.url).toBe('https://www.google.com/maps?q=123,321')
     expect(message.createdAt).toBeA('number');
   });
 });
