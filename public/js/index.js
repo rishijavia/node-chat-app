dd3ffd0015c61fa852c1d7a28950472e8cf4481a
rishijavia <rishi.javia.3895@gmail.com>
2017-01-11 21:15:50 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e){
   e.preventDefault();
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function (){
-
+    messageTextbox.val('');
   });
 });
 
@@ -42,12 +43,16 @@ locationButton.on('click', function() {
     return alert("Geolocation not supported by your browser");
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
+    locationButton.removeAttr('disabled').text('Send Location');
   }, function () {
-    alert("Unable to fetch location");
+    locationButton.removeAttr('disabled');
+    alert("Unable to fetch location").text('Send Location');
   });
 });
